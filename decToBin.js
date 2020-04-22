dfa0415492417327bb6f43ab269e793942333f1e
vd89 <me@daxcodes.in>
2020-04-22 14:52:58 +0530

    the binary to decimal in the dectobin file

--- decToBin.js
+++ decToBin.js
@@ -1,7 +1,7 @@
 // Write a program to Convert Decimal to Binary
-const readlineSync = require('readline-sync');
+// const readlineSync = require('readline-sync');
 
-const num = readlineSync.question("Give the Decimal no to convet to binary : ")
+// const num = readlineSync.question("Give the Decimal no to convet to binary : ")
 
 const toBinary = (n, bin = '') => {
   if (n <= 1) {
@@ -41,5 +41,36 @@ const toBin = (n) => {
 
 }
 
+var num = 98
+var bin = toBin(num)
 console.log(toBinary(num));
 console.log(toBin(num));
+
+
+// Binary to decimal 
+
+const binDec = (n) => {
+  
+  var dec = parseInt(n, 2)
+
+  return dec
+}
+let dec = binDec(bin)
+console.log(dec);
+
+
+function binaryToDecimal(string) {
+  let decimal = +0;
+  let bits = +1;
+  for (let i = 0; i < string.length; i++) {
+    let currNum = +(string[string.length - i - 1]);
+    if (currNum === 1) {
+      decimal += bits;
+    }
+    bits *= 2;
+  }
+ return decimal
+}
+
+let decimal2 = binaryToDecimal(bin)
+console.log(decimal2);
