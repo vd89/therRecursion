a0a29dc92a76850ada8ea70fb9f7d3889f032c86
vd89 <me@daxcodes.in>
2020-05-04 19:20:30 +0530

    the change in the index file for the word coutn and the test subject

--- nodeAssignment/index.js
+++ nodeAssignment/index.js
@@ -1,6 +1,14 @@
+/*
+(1) Write a Node Program to find the number of lines,​ number of words and characters
+in a given input text file.
+(a) Note: File name should be given as command line argument.
+(b) There can be ‘n’ command line arguments as file names.
+*/
+
 const fs = require('fs');
 
 const file = process.argv.splice(2)
+
 
 file.forEach(file => {
     fs.readFile(file, (err, data) => {
@@ -9,8 +17,9 @@ file.forEach(file => {
        } else {
            const fileData = data.toString()
            const line = fileData.split('\n')
-           const word = fileData.split(' ')
-           const chars = fileData.replace(/[\n]+/gm, "").split('')
+           const word = fileData.replace(/[\n]+/gm,' ').split(' ');
+           const chars = fileData.replace(/[\n]+/gm,"").split('')
+           console.log(word);
            console.log(`The number of lines in ${file} are ${line.length}`);
            console.log(`The number of Words in ${file} are ${word.length}`);
            console.log(`The number of characters in ${file} are ${chars.length}`);
