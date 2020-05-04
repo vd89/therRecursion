970cf1b6d58456fa5dfa763c6378b8ba101401e2
vd89 <me@daxcodes.in>
2020-05-04 11:20:24 +0530

    the node fs assignment

---
+++ nodeAssignment/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const file = process.argv.splice(2)
+
+file.forEach(file => {
+    fs.readFile(file, (err, data) => {
+       if (err) {
+           throw err
+       } else {
+           const fileData = data.toString()
+           const line = fileData.split('\n')
+           const word = fileData.split(' ')
+           const chars = fileData.replace(/[\n]+/gm, "").split('')
+           console.log(`The number of lines in ${file} are ${line.length}`);
+           console.log(`The number of Words in ${file} are ${word.length}`);
+           console.log(`The number of characters in ${file} are ${chars.length}`);
+           console.log('\n');
+
+       }
+    })
+})
