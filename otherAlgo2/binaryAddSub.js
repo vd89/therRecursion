5ddb02958b829d3cf0bfe6056910336fbce052fe
vd89 <me@daxcodes.in>
2020-05-11 15:43:56 +0530

    number subtraction using logical operator

--- otherAlgo2/binaryAddSub.js
+++ otherAlgo2/binaryAddSub.js
@@ -13,3 +13,15 @@ let num = 15;
 let num2 = 30;
 
 binAdd(num,num2);
+
+
+const binSub = (x,y) => {
+    while (y !==0) {
+        let borrow = (~x) & y
+        x = x ^ y
+        y = borrow << 1
+    }
+    return x 
+}
+
+console.log(binSub(num2,num));
