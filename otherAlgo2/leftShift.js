1479cf49cb754be6b1c660016d853e99c70f19c0
vd89 <me@daxcodes.in>
2020-05-11 14:28:58 +0530

    The left shift with the formula

---
+++ otherAlgo2/leftShift.js
@@ -0,0 +1,18 @@
+// Write a program using Left Shift Operator (6<<i = 6*2^i) and Derive the General Formula
+
+const leftShift = (n, m) => {
+    // m is the shift and n is given number 
+    let o = n << m
+    let p = n * (2 ** m)
+    
+    if (o == p ) {
+        console.log(`The general formula for left shift is n*(2^m) hence ${o} == ${p}`);
+    } else {
+        console.log(`The given general formula is false`);
+    }
+
+}
+let a = 15
+let b = 10
+
+leftShift(a,b) //The general formula for left shift is n*(2^m) hence 15360 == 15360
